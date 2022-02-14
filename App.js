51f556f6d9f690aa57270c42cc7217de407055b1
Anika Bernstein <anikajb92@gmail.com>
2022-02-14 15:07:38 -0700

    fixed typo in component import

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { useState } from 'react';
-import { TodoCard } from 'TodoCard.js'
+import TodoCard from './TodoCard';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -11,7 +11,7 @@ export default function App() {
   ]);
 
   const showTodos = () => todos.map(todo => {
-    return 
+    return <TodoCard todo={todo}/>
   })
 
   return (
@@ -22,4 +22,14 @@ export default function App() {
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  todoCard: {
+    flex: 1
+  }
+});
--- TodoCard.js
+++ TodoCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function TodoCard({todo}) {
   return (
@@ -8,14 +8,9 @@ export default function TodoCard({todo}) {
     <Text style={styles.content}>{todo.content}</Text>
   </View>
   )
-  
+}
+
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
     todoCard: {
       flex: 1
     },
@@ -26,5 +21,4 @@ export default function TodoCard({todo}) {
   
     }
   });
-}
 
