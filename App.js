c10d691a82ba09b22b3c68e4da504c5828d6ecf8
Anika Bernstein <anikajb92@gmail.com>
2022-02-14 14:28:01 -0700

    created todos hook with first piece of state

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {title: "First Todo", content: "This is my first to do"}
+  ])
+
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
